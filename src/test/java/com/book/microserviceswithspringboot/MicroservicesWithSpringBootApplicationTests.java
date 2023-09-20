ee5368c2ac73de810ef363523d5f2f94fecc36c2
Ricardo <ricardowoj@hotmail.com>
2023-09-20 23:24:35 +0100

    first commit

---
+++ src/test/java/com/book/microserviceswithspringboot/MicroservicesWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.microserviceswithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
